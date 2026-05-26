eb5aaebbc0af7b56bf224e28b8fea6dc7507628a
Thangaprabu <mariappanmsl5@gmail.com>
2026-05-26 17:23:29 +0530

    commit

---
+++ src/main/java/com/prabu/ecommerce/config/MailConfig.java
@@ -0,0 +1,22 @@
+package com.prabu.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("mariappanmsl5@gmail.com");
+        mailSender.setPassword("elmr ltmx abet bedd");
+        mailSender.getJavaMailProperties().put("mail.smtp.auth", "true");
+        mailSender.getJavaMailProperties().put("mail.smtp.starttls.enable", "true");
+        return mailSender;
+    }
+}
