c31ebcd7292b40fe1a32a10400b63a83a3776be5
Thangaprabu <mariappanmsl5@gmail.com>
2026-06-02 14:00:50 +0530

    commit

---
+++ src/main/java/com/prabu/ecommerce/dto/requestdto/RegisterRequestDTO.java
@@ -0,0 +1,28 @@
+package com.prabu.ecommerce.dto.requestdto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequestDTO {
+
+    @NotBlank(message = "name is required")
+    @Size(min = 3, max = 50, message = "name must be between 3 and 50 characters")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    private String password;
+}
